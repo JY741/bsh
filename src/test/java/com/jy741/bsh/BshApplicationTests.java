542ccc1b270f66fe1fa7eaabc3988af8d74c11e9
Jiang Yu <2065373132@qq.com>
2024-07-19 16:40:49 +0800

    first init

---
+++ src/test/java/com/jy741/bsh/BshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy741.bsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
